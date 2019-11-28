5b92f0ae98c028abddd19e25324f21b5ca29665a
Erick Samuel Guerrero <eguerrero@lumston.com>
2019-11-28 10:49:58 -0600

    Add location orgin to redirect adal

--- src/app/components/app-root/app.component.ts
+++ src/app/components/app-root/app.component.ts
@@ -1,3 +1,4 @@
+import { PlatformLocation } from '@angular/common';
 import { environment } from 'src/environments/environment';
 import { AdalService } from 'adal-angular4';
 import { Component } from '@angular/core';
@@ -10,7 +11,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Viva';
   color = '#7EC636';
-  constructor(private adal: AdalService) {
+  constructor(
+    private adal: AdalService,
+    platformLocation: PlatformLocation) {
+    environment.adalConfiguration.redirectUri = (platformLocation as any).location.origin + '/login';
+    environment.adalConfiguration.postLogoutRedirectUri = (platformLocation as any).location.origin + '/login';
     this.adal.init(environment.adalConfiguration);
   }
 }
