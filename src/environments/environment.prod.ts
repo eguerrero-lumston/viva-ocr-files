a86ab860b95064a8b3c187d571ebfb494e17dab1
Erick Samuel Guerrero <eguerrero@lumston.com>
2019-12-16 16:54:05 -0600

    New ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  URL_HOST: 'http://3.211.146.162:3000/api/',
+  URL_HOST: 'http://44.228.27.23:3000/api/',
   adalConfiguration: {
     tenant: 'Vivaaerobus.onmicrosoft.com',
     clientId: '92b14d62-12de-417b-9363-b9f0f0bc8619',
-    redirectUri: 'http://3.211.146.162/login',
-    postLogoutRedirectUri: 'http://3.211.146.162/login',
+    redirectUri: 'http://44.228.27.23/login',
+    postLogoutRedirectUri: 'http://44.228.27.23/login',
     cacheLocation: 'localStorage'
   }
 };
