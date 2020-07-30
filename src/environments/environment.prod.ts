237398dff5c5b92ecb7c23ba23829da00a73eaff
Erick Samuel Guerrero <eguerrero@lumston.com>
2020-07-30 13:03:45 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   adalConfiguration: {
     tenant: 'Vivaaerobus.onmicrosoft.com',
     clientId: '92b14d62-12de-417b-9363-b9f0f0bc8619',
-    redirectUri: 'https://viva-ocvivaexpedientes.com/login',
-    postLogoutRedirectUri: 'https://viva-ocvivaexpedientes.com/login',
+    redirectUri: 'https://vivaexpedientes.com/login',
+    postLogoutRedirectUri: 'https://vivaexpedientes.com/login',
     cacheLocation: 'localStorage'
   }
 };
